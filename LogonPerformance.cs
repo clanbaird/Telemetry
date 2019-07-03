292721e26c120991f26564a2255c2ebf09514543
Joseph Baird <joseph.b.baird.ctr@mail.mil>
2019-07-02 21:00:30 -0600

    initial submission

---
+++ LogonPerformance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Telemetry
+{
+    public class LogonPerformance
+    {
+        private static Stopwatch stopWatch;
+        private static string elapsedTime;
+        public static void Start()
+        {
+            stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+        }
+        public static string StopAndGetResult()
+        {
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            elapsedTime = String.Format("{0:00}:{1:00}.{2:00}",
+                ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            //Console.WriteLine("RunTime " + elapsedTime);
+            return "LogonPerformance: "+ elapsedTime;
+        }
+    }
+}
